710c0a999d374baa20e83a6ac74168b1e01d22e8
nikhil98s <nikhilsrinivas098@gmail.com>
2023-07-21 23:18:47 +0530

    DBMS

---
+++ src/librarian/get_student.php
@@ -0,0 +1,58 @@
+<?php
+	require "../db_connect.php";
+	require "../message_display.php";
+	require "verify_librarian.php";
+	require "header_librarian.php";
+?>
+
+<html>
+	<head>
+		<title>Students</title>
+		<link rel="stylesheet" type="text/css" href="../css/global_styles.css">
+		<link rel="stylesheet" type="text/css" href="../css/custom_checkbox_style.css">
+		<link rel="stylesheet" type="text/css" href="css/pending_registrations_style.css">
+	</head>
+	<body>
+		<?php
+			$query = $con->prepare("SELECT username, name, email, balance FROM member");
+			$query->execute();
+			$result = $query->get_result();
+			$rows = mysqli_num_rows($result);
+			if($rows == 0)
+				echo "<h2 align='center'>No Members/h2>";
+			else
+			{
+				echo "<form class='cd-form' method='POST' action='#'>";
+				echo "<center><legend>Members</legend></center>";
+				echo "<div class='error-message' id='error-message'>
+						<p id='error'></p>
+					</div>";
+				echo "<table width='100%' cellpadding=10 cellspacing=10>
+						<tr>
+							<th>Username<hr></th>
+							<th>Name<hr></th>
+							<th>Email<hr></th>
+							<th>Balance<hr></th>
+						</tr>";
+				for($i=0; $i<$rows; $i++)
+				{
+					$row = mysqli_fetch_array($result);
+					echo "<tr>";
+
+					$j;
+					for($j=0; $j<3; $j++)
+						echo "<td>".$row[$j]."</td>";
+					echo "<td>$".$row[$j]."</td>";
+					echo "</tr>";
+				}
+				echo "</table><br /><br />";
+				echo "<div style='float: right;'>";
+				echo "<input type='submit' onClick='window.print()' value='Print' name='l_print' />";
+				echo "</div>";
+				echo "</form>";
+			}
+			
+			$header = 'From: <noreply@library.com>' . "\r\n";
+		?>
+	</body>
+</html>
